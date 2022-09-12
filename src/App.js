e36cecab0b566454f4a03dd4e79538143ea17118
amrashour <a.ashour@msd-my.com>
2022-09-12 20:04:33 +0200

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+//react router
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+//components
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import Footer from './components/footer/Footer';
+import MovieDetail from './components/movieDetails/MovieDetail';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+
+//styles
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route path='/' exact element={<Home />} />
+            <Route path="/movie/:imdbID" element={<MovieDetail />} />
+            <Route element={<PageNotFound />} />
+          </Routes> 
+        </div> 
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/common/APIs/movieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey = "ecb66602";
